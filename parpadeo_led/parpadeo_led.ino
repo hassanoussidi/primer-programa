ca3c7efb001cdc36d69184078de3234e4d66a4c5
hassanoussidi <assan1@outlook.es>
2016-02-20 18:07:43 +0100

    parpadeo LED

---
+++ parpadeo_led/parpadeo_led.ino
@@ -0,0 +1,27 @@
+
+
+/***********************************************
+**                                            **
+**            parpadeo  led                   **
+**               pin13                        **
+***********************************************/
+
+//******  Includes  *****************************
+
+
+//******  Variables  ****************************
+int ledPin = 13;
+
+
+void setup() {
+
+  pinMode(ledPin, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
